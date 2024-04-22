a8eb1792b576cff1e5977e3fd133470279f5d586
FutureShock314 <ayaan.waqas@outlook.com>
2024-04-22 20:16:11 +0100

    FEAT: Set up home page (buttons dont work)

--- bcpshop2/app/page.tsx
+++ bcpshop2/app/page.tsx
@@ -1,9 +1,42 @@
+import styles from "./home.module.css"
+import bg from './gradient.png'
+
 export default function Home() {
   return (
-    <main>
-      <div className="fish">
+    <main className={ styles.main }>
+      {/* <div className="fish">
         <h1>Hello!</h1>
-      </div>
+      </div> */}
+      <div className={ styles.land } style={{ backgroundImage: `url(${bg.src})` }}> {/* style needed for bg img to work */}
+            <div className={ styles.top }>
+                <h1 className={ styles.top_title }>WELCOME TO</h1>
+            </div>
+
+            <div className={ styles.bottom }>
+                <h1 className={ styles.bottom_title } style={{ backgroundImage: `url(${bg.src})` }}>THE BCP SHOP</h1>
+            </div>
+        </div>
+
+        <div className={ styles.login_wrapper }>
+            <div
+                className={`${ styles.button } ${ styles.login }`}
+                // onclick="window.location='./login/'"
+            >Log In</div>
+        </div>
+
+        <div className={ styles.sign_up_wrapper}>
+            <div
+                className={`${ styles.button } ${ styles.sign_up }`}
+                // onclick="window.location='./login/'"
+            >Sign Up</div>
+        </div>
+
+        <style>{`
+          html,
+          body {
+            overflow: hidden;
+          }
+        `}</style>
     </main>
   );
 }
