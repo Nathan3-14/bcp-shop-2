d3769382577faa57fd0a157deeef567f53fc8a32
FutureShock314 <117740939+FutureShock314@users.noreply.github.com>
2024-04-23 14:29:30 +0000

    fix again

--- app/page.tsx
+++ app/page.tsx
@@ -4,41 +4,40 @@ import Link from "next/link"
 
 export default function Home() {
   return (
-    <div className={ styles.main }>
-      {/* <div className="fish">
-        <h1>Hello!</h1>
-      </div> */}
-      <div className={ styles.land } style={{ backgroundImage: `url(${bg.src})` }}> {/* style needed for bg img to work */}
-            <div className={ styles.top }>
-                <h1 className={ styles.top_title }>WELCOME TO</h1>
+    <>
+      <div className={styles.main}>
+        {/* <div className="fish">
+          <h1>Hello!</h1>
+          </div> */}
+          <div className={styles.land} style={{ backgroundImage: `url(${bg.src})` }}> {/* style needed for bg img to work */}
+              <div className={styles.top}>
+                  <h1 className={styles.top_title}>WELCOME TO</h1>
+              </div>
+
+              <div className={styles.bottom}>
+                  <h1 className={styles.bottom_title} style={{ backgroundImage: `url(${bg.src})` }}>THE BCP SHOP</h1>
+              </div>
+          </div>
+
+          <div className={styles.login_wrapper}>
+              <Link
+                  className={`${styles.button} ${styles.login}`}
+                  // onclick="window.location='./login/'"
+                  href="/login"
+              >Log In</Link>
+          </div>
+          <div className={styles.sign_up_wrapper}>
+            <div className={`${styles.button} ${styles.sign_up}`}>
+              Sign Up
             </div>
-
-            <div className={ styles.bottom }>
-                <h1 className={ styles.bottom_title } style={{ backgroundImage: `url(${bg.src})` }}>THE BCP SHOP</h1>
-            </div>
-        </div>
-
-        <div className={ styles.login_wrapper }>
-            <Link
-                className={`${ styles.button } ${ styles.login }`}
-                // onclick="window.location='./login/'"
-                href="/login"
-            >Log In</div>
-        </div>
-
-        <div className={ styles.sign_up_wrapper}>
-            <div
-                className={`${ styles.button } ${ styles.sign_up }`}
-                // onclick="window.location='./login/'"
-            >Sign Up</div>
-        </div>
-
-        <style>{`
-          html,
-          body {
-            overflow: hidden;
-          }
-        `}</style>
-    </div>
+          </div>
+          <style>{`
+            html,
+            body {
+              overflow: hidden;
+            }
+          `}</style>
+      </div>
+    </>
   );
 }
