057dd7b47c8cc1b18b2071a528b0213063dd3df5
FutureShock314 <ayaan.waqas@outlook.com>
2024-04-24 19:25:15 +0100

    base login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,15 @@
 import styles from "@/styles/login/login.module.css"
+import Canvas from "@/app/components/Canvas"
 
 export default function LoginPage() {
   return (
     <>
-      <h1>Login</h1>
+      <Canvas />
+      <div className={ styles.login_wrapper }>
+        <div className={ styles.login_window }>
+        
+        </div>
+      </div>
     </>
   )
 }
