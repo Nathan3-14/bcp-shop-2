319ce418cb7f9a73c033ae977b46de9518f479a4
FutureShock314 <117740939+FutureShock314@users.noreply.github.com>
2024-04-24 15:11:50 +0000

    idk what im doing anymore ill fix this at some point later

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,9 @@
-import React, { useEffect, useState } from 'react';
 import styles from "@/styles/home.module.css"
 import bg from '@/styles/gradient.png'
 import Link from "next/link"
-import dynamic from "next/dynamic"
 // import Gradient from "@/app/lib/gradient.js"
 
-var gradient = dynamic(() => (import("@/app/lib/gradient.js")), { ssr: false })
-
 export default function Home() {
-  useEffect(() => {
-    gradient.then((module) => {
-      const Gradient = module.default;
-      const grad = new Gradient();
-      grad.initGradient("#gradient-canvas");
-    });
-  }, []);
-
   return (
     <>
       <div className={styles.main} id={styles.gradient_canvas}>
