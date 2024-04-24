fb87eb60718f656e2b61dc85d0349c48a5414a36
FutureShock314 <117740939+FutureShock314@users.noreply.github.com>
2024-04-24 15:05:10 +0000

    this wont work

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import React, { useEffect, useState } from 'react';
 import styles from "@/styles/home.module.css"
 import bg from '@/styles/gradient.png'
 import Link from "next/link"
 import dynamic from "next/dynamic"
 // import Gradient from "@/app/lib/gradient.js"
 
-var gradient = dynamic(() => import("@/app/lib/gradient.js"), { ssr: false })
+var gradient = dynamic(() => (import("@/app/lib/gradient.js")), { ssr: false })
 
 export default function Home() {
   useEffect(() => {
@@ -21,9 +22,7 @@ export default function Home() {
         {/* <div className="fish">
           <h1>Hello!</h1>
           </div> */}
-        <div className={styles.land} style={
-          {/* { backgroundImage: `url(${bg.src})` } */}
-        }> {/* style needed for bg img to work */}
+        <div className={styles.land}> {/* style needed for bg img to work */}
           <div className={styles.top}>
             <h1 className={styles.top_title}>WELCOME TO</h1>
           </div>
