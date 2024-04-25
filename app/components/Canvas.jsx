4ab2c8f925f3f1cfc0a42950689bbe1797ab58a1
FutureShock314 <117740939+FutureShock314@users.noreply.github.com>
2024-04-25 09:12:48 +0000

    test

--- app/components/Canvas.jsx
+++ app/components/Canvas.jsx
@@ -4,45 +4,45 @@ import React, { useRef, useEffect, ReactNode } from "react";
 import { Gradient } from "@/app/lib/StripeGradient";
 
 const Canvas = (props) => {
-	const canvasRef2 = useRef(null);
-	const gradient = new Gradient();
+  const canvasRef2 = useRef(null);
+  const gradient = new Gradient();
 
-	useEffect(() => {
-		gradient.initGradient("#gradient-canvas");
-	}, []);
+  useEffect(() => {
+    gradient.initGradient("#gradient-canvas");
+  }, []);
 
-	var colors = props.colors.split(' ')
-	console.log(colors)
-	console.log(colors[0])
+  var colors = props.colors.split(' ')
+  console.log(colors)
+  console.log(colors[0])
 
-	return (
-		<>
-			<canvas
-				className=''
-				id="gradient-canvas"
-				ref={canvasRef2}
-				{...props}
-				data-transition-in
-			>
-				{props.children}
-			</canvas>
+  return (
+    <>
+      <canvas
+        className=''
+        id="gradient-canvas"
+        ref={canvasRef2}
+        {...props}
+        data-transition-in
+      >
+        {props.children}
+      </canvas>
 
-			<style>{`
-				#gradient-canvas {
-					--gradient-color-1: ${ colors[0] ? colors[0] : '#000' };
-					--gradient-color-2: ${ colors[1] ? colors[1] : '#000' };
-					--gradient-color-3: ${ colors[2] ? colors[2] : '#000' };
-					--gradient-color-4: ${ colors[3] ? colors[3] : '#000' };
+      <style>{`
+        #gradient-canvas {
+          --gradient-color-1: ${ colors[0] ? colors[0] : '#000' };
+          --gradient-color-2: ${ colors[1] ? colors[1] : '#000' };
+          --gradient-color-3: ${ colors[2] ? colors[2] : '#000' };
+          --gradient-color-4: ${ colors[3] ? colors[3] : '#000' };
 
-					height: 100vh;
-					width: 100%;
-				
-					position: absolute;
-					z-index: -10;
-				}
-			`}</style>
-		</>
-	);
+          height: 100dvh;
+          width: 100%;
+        
+          position: absolute;
+          z-index: -10;
+        }
+      `}</style>
+    </>
+  );
 };
 
 export default Canvas;
